6c5bf2bf7da2e77f4492886d5294e838d4da1550
Shawn Moss <shawnrmoss@gmail.com>
2015-08-27 17:30:49 -0600

    Made movie class parceble and passed movie object to detail activity with via intent.

--- app/src/main/java/com/shawnrmoss/popularmovies/DetailActivityFragment.java
+++ app/src/main/java/com/shawnrmoss/popularmovies/DetailActivityFragment.java
@@ -1,12 +1,27 @@
 package com.shawnrmoss.popularmovies;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.text.style.BackgroundColorSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.sql.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -24,9 +39,67 @@ public class DetailActivityFragment extends Fragment {
 
         Movie movie = getActivity().getIntent().getParcelableExtra("Movie");
 
-        ((TextView) rootView.findViewById(R.id.movie_title))
+        ((TextView) rootView.findViewById(R.id.title))
                 .setText(movie.getTitle());
 
+        ((TextView) rootView.findViewById(R.id.release_date))
+                .setText(movie.getRelease_date().substring(0,4));
+
+        ((TextView) rootView.findViewById(R.id.vote_average))
+                .setText(Double.toString(movie.getVote_average()) + "/10");
+
+        ((TextView) rootView.findViewById(R.id.plot_synopsis))
+                .setText(movie.getPlot_synopsis());
+
+        ImageView imageView = ((ImageView) rootView.findViewById(R.id.movie_poster));
+        Picasso.with(rootView.getContext()).load(buildMovie_PosterURL(movie.getMovie_poster(), 3)).into(imageView);
+
+        getActivity().setTitle("Movie Details");
+
+
         return rootView;
+    }
+
+    public String buildMovie_PosterURL(String url, int imageSize) {
+        //http://image.tmdb.org/t/p/w185//nBNZadXqJSdt05SHLqgT0HuC5Gm.jpg
+        //"w92", "w154", "w185", "w342", "w500", "w780", or "original". For most phones we recommend using "w185"
+        String size;
+        switch (imageSize) {
+            case 0:
+                size = "w92";
+                break;
+            case 1:
+                size = "w154";
+                break;
+            case 2:
+                size = "w185";
+                break;
+            case 3:
+                size = "w342";
+                break;
+            case 4:
+                size = "w500";
+                break;
+            case 5:
+                size = "w780";
+                break;
+            case 6:
+                size = "original";
+                break;
+            default:
+                size = "w185";
+                break;
+        }
+
+
+        Uri.Builder builder = new Uri.Builder();
+        builder.scheme("http")
+                .authority("image.tmdb.org")
+                .appendPath("t")
+                .appendPath("p")
+                .appendPath(size)
+                .appendEncodedPath(url);
+
+        return builder.build().toString();
     }
 }
--- app/src/main/java/com/shawnrmoss/popularmovies/MainActivityFragment.java
+++ app/src/main/java/com/shawnrmoss/popularmovies/MainActivityFragment.java
@@ -11,7 +11,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.GridView;
-import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
